7d96dee1e3437c1d166bb6b6e8fad927e86b290e
forumber <kidobeko@gmail.com>
2022-03-04 23:55:26 +0300

    Add onclick listener to register button

--- app/src/main/java/com/forumber/tokencasestudy/LoginActivity.kt
+++ app/src/main/java/com/forumber/tokencasestudy/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.forumber.tokencasestudy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.forumber.tokencasestudy.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonRegister.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
